e79b7552cdf70af2a6280f97f7e843fb25a5eb91
River Kuo <coco0958231520@gmail.com>
2022-07-01 23:23:58 +0800

    callback function add code

--- week-4/callback-function/callback-function-and-advanced-html-dom.js
+++ week-4/callback-function/callback-function-and-advanced-html-dom.js
@@ -1,10 +1,28 @@
+function ajax(src, callback) {
+    //your code here
+    let xhr = new XMLHttpRequest();
+      
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            let response = JSON.parse(xhr.responseText);
+            // console.log(response); //map or foreach
+            // return response
+            console.log(response);
+            callback(response);
+        }
+    };
+
+    xhr.open("GET", src);
+    xhr.send();
+}
+
 function render(data) {
     // your code here
     const newData = data
         .map(product => `${product.name} 要價新台幣${product.price}元，是${product.description}<br>`)
         .join("");
         document.getElementById("body").innerHTML = newData;
-    }
+}
     
     
 ajax("https://appworks-school.github.io/Remote-Aassigiment-Data/products", function(response) {
